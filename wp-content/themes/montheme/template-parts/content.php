bda5bc08a931d5df7045ae9b621b9e6cb7ecc53e
Blanchet Romain <r.blanchet@itakademy.fr>
2018-01-23 15:32:44 +0100

    Page accueil finit

--- wp-content/themes/montheme/template-parts/content.php
+++ wp-content/themes/montheme/template-parts/content.php
@@ -8,56 +8,32 @@
  */
 
 ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<div class="card">
-
-			<!--Card image-->
-			<?php montheme_post_thumbnail(); ?>
+<div class="col-md-4">
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<header class="entry-header">
+			<div class="card">
+				<!--Card image-->
+				<?php montheme_post_thumbnail(); ?>
+				<?php if ( 'post' === get_post_type() ) : ?>
+					<div class="card-body">
+							<!--Title-->
+							<?php
+							if ( is_singular() ) :
+								the_title( '<h4 class="card-title">', '</h4>' );
+							else :
+								the_title( '<h4 class="card-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h4>' );
+							endif;
+							?>
+							<!--Text-->
+							<?php
+								$content_post = get_post();
+								$content = $content_post->post_content;
+							?>
+							<p class="card-text"><?php echo substr($content, 0, 50); ?> ... </p>
+							<a href="#" class="btn btn-primary">Lire la suite (CA MARCHE PAS)</a>
+					</div>
 			<?php
-			if ( is_singular() ) :
-				the_title( '<h4 class="card-title">', '</h4>' );
-			else :
-				the_title( '<h4 class="card-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h4>' );
-			endif;
-
-			if ( 'post' === get_post_type() ) : ?>
-				<div class="card-body">
-						<!--Title-->
-						<h4 class="card-title">Card title</h4>
-						<!--Text-->
-						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						<a href="#" class="btn btn-primary">Button</a>
-				</div>
-		<?php
-		endif; ?>
-	</header><!-- .entry-header -->
-
-
-
-	<div class="entry-content">
-		<?php
-			the_content( sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'montheme' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				get_the_title()
-			) );
-
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'montheme' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php montheme_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-<?php the_ID(); ?> -->
+			endif; ?>
+		</header><!-- .entry-header -->
+	</article><!-- #post-<?php the_ID(); ?> -->
+</div>
