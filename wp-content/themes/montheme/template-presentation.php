54a11b1195625533648870bafcf934f6171a43a8
DavidCaraiannis <david.caraiannis@gmail.com>
2018-01-23 14:54:17 +0100

    debut page presentation

---
+++ wp-content/themes/montheme/template-presentation.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Template Name: presentation
+*/
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+			SURPRISE MOTHERFUCKER
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
